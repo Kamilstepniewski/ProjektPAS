91928ef273d8a3167056bbd08df4a6274f35e780
Kamil Stępniewski <kamil.stepniewski@outlook.com>
2020-06-09 19:17:29 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import ssl
 import requests
 import tkinter as tk
-import  uuid
+import uuid
+import numpy as np
 
 root = tk.Tk()
 root.title("Tic Tac Toe")
@@ -11,150 +12,159 @@ root.geometry("600x600")
 
 root['background'] = '#00ace6'
 
-bclick = False
-
 # requests.get("http://127.0.0.1", verify=r'C:\Users\admin\Downloads\server.crt')
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH,cafile = r'C:\Users\admin\Downloads\server.crt')
-context.load_cert_chain(certfile= r'C:\Users\admin\Downloads\client.crt', keyfile= r'C:\Users\admin\Downloads\client.key')
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=r'C:\Users\admin\Downloads\server.crt')
+context.load_cert_chain(certfile=r'C:\Users\admin\Downloads\client.crt', keyfile=r'C:\Users\admin\Downloads\client.key')
+
+
 # context.load_verify_locations(cafile=r'C:\Users\admin\Downloads\client.crt')
 
-#Tablica
+# Tablica
 
-#|7|8|9|
-#|4|5|6|
-#|1|2|3|
+# |7|8|9|
+# |4|5|6|
+# |1|2|3|
 
 # Struktura wiadomości
-#'To:\r\n
-#From:\r\n
-#Information_about_client_sesion_id:\r\n
-#Message_id:\r\n
-#Content-length:\r\n
-#Message:\r\n\r\n
+# 'To:\r\n
+# From:\r\n
+# Information_about_client_sesion_id:\r\n
+# Message_id:\r\n
+# Content-length:\r\n
+# Message:\r\n\r\n
+
+def opakuj(To, From, Information_about_client_sesion_id, Message_id, Content_length, Message):
+    return f"To:{To}\r\nFrom:{From}\r\nInformation_about_client_sesion_id:{Information_about_client_sesion_id}\r\nMessage_id:{Message_id}\r\n\Content_length\r\n{Content_length}\r\nMessage{Message}"
+
 
 def read_message(message):
     # Tutaj można odbierać wiadomość dopóki b'\r\n\r\n' not in data
-    #data = b''
-    #while b'\r\n\r\n' not in data:
+    # data = b''
+    # while b'\r\n\r\n' not in data:
     #    data += client.recv(1)
-    To = message[message[0:].find("To:")+3:message.find('\r\nFrom:')]
-    From = message[message[0:].find("From:")+5:message.find('\r\nInformation_about_client_sesion_id:')]
-    Information_about_client_sesion_id = message[message[0:].find("Information_about_client_sesion_id:") + 35:message.find('\r\nMessage_id:')]
+    To = message[message[0:].find("To:") + 3:message.find('\r\nFrom:')]
+    From = message[message[0:].find("From:") + 5:message.find('\r\nInformation_about_client_sesion_id:')]
+    Information_about_client_sesion_id = message[
+                                         message[0:].find("Information_about_client_sesion_id:") + 35:message.find(
+                                             '\r\nMessage_id:')]
     Message_id = message[message[0:].find("Message_id:") + 11:message.find('\r\nContent-length:')]
     Content_length = int(message[message[0:].find("Content-length:") + 15:message.find('\r\nMessage:')])
-    #len = 8+Content_length
+    # len = 8+Content_length
     Message = message[message[0:].find("Message:") + 8:message.find('\r\n\r\n')]
 
-    print(To,From,Information_about_client_sesion_id,Message_id,Content_length,Message)
+    return To, From, Information_about_client_sesion_id, Message_id, Content_length, Message
 
 
-#Funkcja nasłuchująca wiadomość od serwera
-def nasłuchuj_serwer():
-    data =''
+# Funkcja nasłuchująca wiadomość od serwera
+def nasluchuj_serwer():
+    data = b""
     while data:
         data += client.recv(1)
     return data
 
-#Będziemy wysyłać poszczególne cyfry poprzez ten sposób będziemy wiedzieć które pole skreślił klient
 
-#Funkcja która będzie zmieniać stan buttona tak aby wyświetlić ruch otrzymany od serwera
-#def change_state_button_from_server(button1,button2,button3,button4,button5,button6,button7,button8,button9,number):
+# Będziemy wysyłać poszczególne cyfry poprzez ten sposób będziemy wiedzieć które pole skreślił klient
+
+# Funkcja która będzie zmieniać stan buttona tak aby wyświetlić ruch otrzymany od serwera
+# def change_state_button_from_server(button1,button2,button3,button4,button5,button6,button7,button8,button9,number):
 #    pass
 gra_rozpoczeta = False
+
+
 def set_plansza(plansza):
     global aktualny_stan_planszy
     for znak in range(9):
         aktualny_stan_planszy[znak] = plansza[znak]
 
-def button_click(button,number,aktualny_stan_planszy,list_of_button):
+
+def button_click(button, number, aktualny_stan_planszy, list_of_button):
     global bclick
     if button["text"] == " ":
         button["text"] = 'X'
         bclick = True
         button["state"] = 'normal'
         button["state"] = 'disabled'
-        aktualny_stan_planszy[number-1] = "X"
+        aktualny_stan_planszy[number - 1] = "X"
         print(aktualny_stan_planszy)
 
-        #Serwer czeka na ruch więc muszę mu wysłać aktualny stan planszy i ruch <- wystarczy że wyślesz numer ruch
+        # Serwer czeka na ruch więc muszę mu wysłać aktualny stan planszy i ruch <- wystarczy że wyślesz numer ruch
         ruch = str(number)
-#         msg_stan_planszy_ruch= "".join(aktualny_stan_planszy)+"\n\n"+ruch
+        #         msg_stan_planszy_ruch= "".join(aktualny_stan_planszy)+"\n\n"+ruch
         client.sendall(ruch.encode())
 
-        #Jeżeli ruch prawidłowy to sprawdź co mówi serwer
-        #Czy koniec gry i jaki wynik - ok
-        #ok2 - przyjął ruch ale gra jeszcze nie jest zakonczona
-        #ok0
-        #ok1 gra wygrana lub przerwana
+        # Jeżeli ruch prawidłowy to sprawdź co mówi serwer
+        # Czy koniec gry i jaki wynik - ok
+        # ok2 - przyjął ruch ale gra jeszcze nie jest zakonczona
+        # ok0
+        # ok1 gra wygrana lub przerwana
         global gra_rozpoczeta
         if not gra_rozpoczeta:
-            msg =b''
+            msg = b''
             while True:
                 data = client.recv(1)
-                print("I receive:" + data.decode() )
+                print("I receive:" + data.decode())
                 msg += data
                 if msg == b'i am ready':
                     break
-            msg =b''
+            msg = b''
             client.sendall(b"START")
             while True:
                 data = client.recv(1)
-                print("I receive:" + data.decode() )
+                print("I receive:" + data.decode())
                 msg += data
                 if msg == b'ok':
-                    
                     gra_rozpoczeta = True
                     break
-                
+
         if gra_rozpoczeta:
-            msg =b''
+            msg = b''
             while True:
                 data = client.recv(1)
-                print("I receive:" + data.decode() )
+                print("I receive:" + data.decode())
                 msg += data
                 if msg == b'musisz poczekac':
                     msg = b""
                     print("czekam")
                     client.sendall(b"ok")
-                    msg =b''
+                    msg = b''
                     pass
-                
+
                 if msg[:10] == b"podaj ruch" and len(msg) == 19:
                     plansza = msg[10:]
                     plansza = plansza.decode()
                     for i in range(9):
-                        if plansza[i]!= " ":
-                            list_of_button[i+1]["text"] = plansza[i]
-                            list_of_button[i+1]["state"] = 'normal'
-                            list_of_button[i+1]["state"] = 'disabled'
+                        if plansza[i] != " ":
+                            list_of_button[i + 1]["text"] = plansza[i]
+                            list_of_button[i + 1]["state"] = 'normal'
+                            list_of_button[i + 1]["state"] = 'disabled'
                             aktualny_stan_planszy[i] = plansza[i]
                         client.sendall(ruch.encode())
                     print("wykonałem ruch")
                     break
-            msg =b''
+            msg = b''
             while True:
                 data = client.recv(1)
-                print("I receive:" + data.decode() )
-                
+                print("I receive:" + data.decode())
+
                 msg += data
                 print(msg)
-                if(msg == b'nieprawidlowy ruch'):
+                if (msg == b'nieprawidlowy ruch'):
                     button["text"] = ' '
                     bclick = False
                     button["state"] = 'normal'
-                    aktualny_stan_planszy[number-1] = " "
+                    aktualny_stan_planszy[number - 1] = " "
                     return
-                if(msg == b'ok1'):
+                if (msg == b'ok1'):
                     print("Wygrana")
                     return
-                elif(msg == b'ok0'):
+                elif (msg == b'ok0'):
                     print("Przegrana")
                     return
-                elif(msg==b'ok2'):
-                    #Gra toczy się dalej
-                    #odczytaj ruch
-                    #Tutaj najlepiej jakby serwer wysłał numer(numer potrzebny do zaktualizowania planszy)wtedy:
-                    #Jeśli wysłał ok2 to powinien wysłać numer ruchu
+                elif (msg == b'ok2'):
+                    # Gra toczy się dalej
+                    # odczytaj ruch
+                    # Tutaj najlepiej jakby serwer wysłał numer(numer potrzebny do zaktualizowania planszy)wtedy:
+                    # Jeśli wysłał ok2 to powinien wysłać numer ruchu
 
                     msg = client.recv(1)
                     msg = int(msg.decode())
@@ -169,106 +179,77 @@ def button_click(button,number,aktualny_stan_planszy,list_of_button):
                     break
 
 
-
-
-
-
 def New_game():
     global aktualny_stan_planszy
-    
+
     aktualny_stan_planszy = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    Session_id = uuid.uuid4()
+
     msg_start = "START"
 
-    #client.sendall(msg_start.encode())
+    login = np.random.randint(100, 999)
+
+    msg_start = f'To:Server\r\nLogin:{login}\r\nContent-length:5\r\nMessage:START\r\n\r\n'
+
+    client.sendall(msg_start.encode())
+
+    # client.sendall(msg_start.encode())
 
     # Czy mogę zacząć grę?
 
-    #od_serwera = client.recv(4024)
+    # od_serwera = client.recv(4024)
 
     od_serwera = 'ok'
 
-    if od_serwera == 'ok':
-        #client.sendall("Startuje")
-
-        print("Move to window where")
-        window = tk.Toplevel(root)
-        window.geometry("600x600")
-        window['background'] = '#00ace6'
-
-        label = tk.Label(window, text="Your name:", font='Helvetica', bg='#00ace6', fg='black', height=2, width=14)
-        label.grid(row=1, column=0)
-
-        p1 = tk.StringVar()
-        player1_name = tk.Entry(window, textvariable=p1, bd=9)
-        player1_name.grid(row=1, column=1, columnspan=2)
-
-        button7 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button7,7,aktualny_stan_planszy,list_of_button))
-        button7.grid(row=2, column=1)
-
-        button8 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button8,8,aktualny_stan_planszy,list_of_button))
-        button8.grid(row=2, column=2)
-
-        button9 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button9,9,aktualny_stan_planszy,list_of_button))
-        button9.grid(row=2, column=3)
-
-        button4 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button4,4,aktualny_stan_planszy,list_of_button))
-        button4.grid(row=3, column=1)
-
-        button5 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button5,5,aktualny_stan_planszy,list_of_button))
-        button5.grid(row=3, column=2)
-
-        button6 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button6,6,aktualny_stan_planszy,list_of_button))
-        button6.grid(row=3, column=3)
-
-        button1 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button1,1,aktualny_stan_planszy,list_of_button))
-        button1.grid(row=4, column=1)
-
-        button2 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button2,2,aktualny_stan_planszy,list_of_button))
-        button2.grid(row=4, column=2)
-
-        button3 = tk.Button(window, text=" ", font='Times 20 bold', bg='white', fg='white', height=4, width=8,
-                            command=lambda: button_click(button3,3,aktualny_stan_planszy,list_of_button))
-        button3.grid(row=4, column=3)
-
-        # button_new_game = tk.Button(window, text="New game ", font='Times 20 bold', bg='white', height=4, width=8)
-        # button_new_game.grid(row=5, column=3)
-        # img_1 = tk.PhotoImage(file="C:/Users/admin/Desktop/TICtacTOE/unnamed_2.png")
-        button_new_game = tk.Button(window, text="New game", bg='white', width=18, height=3, command=New_game)
-
-        # img_1 = tk.PhotoImage(file="C:/Users/admin/Desktop/TICtacTOE/unnamed_2.png")
-        # button_new_game.config(image=img_1)
-        button_new_game.grid(row=2, column=0)
-
-        list_of_button = [button_new_game,button1,button2,button3,button4,button5,button6,button7,button8,button9]
-
 
 import socket
+
 SERVER = "127.0.0.1"
 PORT = 8082
-with socket.create_connection((SERVER,PORT)) as sock:
-
+with socket.create_connection((SERVER, PORT)) as sock:
     with context.wrap_socket(sock, server_hostname="localhost") as client:
-        print(client.version())
-#         client.sendall(b"cos tam")
-        print("connected")
+        session_id = None
+        while True:
+            command = str(input("podaj komendę"))
+            if command == "start":
+                login = np.random.randint(100, 999)
+                msg_start = f'To:Server\r\nLogin:{login}\r\nContent-length:5\r\nMessage:START\r\n\r\n'
+                client.sendall(msg_start.encode())
+                print(msg_start)
+                resp = nasluchuj_serwer()
+                resp = resp.decode()
+                print("test -1 ")
+                if resp != "code:400 login failed":
+                    if resp[3:6] == str(login):
+                        session_id = resp[25:-4]
+                        msg = opakuj("SERWER", login, session_id, 100, len("i am ready"), "i am ready")
+                        client.sendall(msg.encode())
+                        while True:
+                            resp = nasluchuj_serwer()
+                            resp = resp.decode()
+                            if resp != "code:401 Timeout":# zmieńmy to może na to że jak nie dostaniejsz odpowiedzi w ciągu x sekund to masz timeout
+                                if resp[3:6] == str(login) and session_id == resp[25:-4]: # nie wiem czy chcemy sprawdzać swoje session_id
+                                    command = str(input("Podaj ruch:"))
+                                    #Tutaj urzytkownik poda ruch i wyśle liczbę, serwer zwaliduje czy była ona poprawna
+                                    msg_ruch = opakuj("SERWER",login,session_id,100,len(command),command)
+                                    client.sendall(msg_ruch.encode())
+                                    #Czekaj na odpowiedz serwera o ruchu
+                                    resp = nasluchuj_serwer()
+                                    resp = resp.decode()
+                                    if resp[3:6] == str(login) and session_id == resp[25:-4]:
+                                        if resp[-12:-4] == "BAD MOVE":
+                                            print("BAD MOVE")
+                                        if resp[-14:-4] == "RIGHT MOVE":
+                                            print("RIGHT MOVE")
+                                            #Dobry ruch więc czekam na swoją kolej
 
 
-        b = tk.Button(root, text="New game", command=New_game)
-        b.config(width=300, height=100)
 
-#         img = tk.PhotoImage(file="C:/Users/admin/Desktop/TICtacTOE/unnamed.png")
-#         b.config(image=img)
-        b.pack(expand=1)
-        #root.update()
-        root.mainloop()
+
+
+                    else:
+                        msg = f'To:Server\r\nLogin:{login}\r\nContent-length:5\r\nMessage:BAD CREDENTIALS\r\n\r\n'
+                        client.sendall(msg.encode())
+                else:
+                    print("nie powiodło się logowanie")
 
         client.close()
--- server.py
+++ server.py
@@ -1,2 +1,260 @@
-#Server
-int x
+# SERWER
+import ssl
+import uuid
+
+import numpy as np
+import socket, threading
+import time
+
+
+def opakuj(To, From, Information_about_client_sesion_id, Message_id, Content_length, Message):
+    return f"To:{To}\r\nFrom:{From}\r\nInformation_about_client_sesion_id:{Information_about_client_sesion_id}\r\nMessage_id:{Message_id}\r\n\Content_length\r\n{Content_length}\r\nMessage{Message}"
+
+
+def read_message(message):
+    # Tutaj można odbierać wiadomość dopóki b'\r\n\r\n' not in data
+    # data = b''
+    # while b'\r\n\r\n' not in data:
+    #    data += client.recv(1)
+    To = message[message[0:].find("To:") + 3:message.find('\r\nFrom:')]
+    From = message[message[0:].find("From:") + 5:message.find('\r\nInformation_about_client_sesion_id:')]
+    Information_about_client_sesion_id = message[
+                                         message[0:].find("Information_about_client_sesion_id:") + 35:message.find(
+                                             '\r\nMessage_id:')]
+    Message_id = message[message[0:].find("Message_id:") + 11:message.find('\r\nContent-length:')]
+    Content_length = int(message[message[0:].find("Content-length:") + 15:message.find('\r\nMessage:')])
+    # len = 8+Content_length
+    Message = message[message[0:].find("Message:") + 8:message.find('\r\n\r\n')]
+
+    return To, From, Information_about_client_sesion_id, Message_id, Content_length, Message
+
+
+plansza = np.array([[0, 0, 0] for i in range(3)])
+aktualny_gracz = 1
+
+ostatni_ruch = -1
+
+
+def set_ostatni_ruch(number):
+    global ostatni_ruch
+    ostatni_ruch = number
+
+
+def reset_planszy():
+    global plansza
+    plansza = np.array([[0, 0, 0] for i in range(3)])
+
+
+def aktualny_gracz_f():
+    return aktualny_gracz
+
+
+def get_plansza():
+    return plansza.copy()
+
+
+def set_plansza(new_plansza):
+    global plansza
+    plansza = new_plansza
+
+
+def wykonaj_ruch(x, y, gracz):
+    plansza = get_plansza()
+    if plansza[x][y] != 0:
+        return "pole zajete"
+    else:
+        plansza[x][y] = gracz
+        set_plansza(plansza)
+    return "udalo sie"
+
+
+def zmien_gracza():
+    global aktualny_gracz
+    aktualny_gracz = (-1) * aktualny_gracz
+
+
+def tlumacz_na_x_y(ruch):
+    if ruch == 1:
+        return 0, 0
+    if ruch == 2:
+        return 0, 1
+    if ruch == 3:
+        return 0, 2
+    if ruch == 4:
+        return 1, 0
+    if ruch == 5:
+        return 1, 1
+    if ruch == 6:
+        return 1, 2
+    if ruch == 7:
+        return 2, 0
+    if ruch == 8:
+        return 2, 1
+    if ruch == 9:
+        return 2, 2
+    else:
+        return None
+
+
+def czy_koniec():
+    for i in range(3):
+        if plansza[i][0] == plansza[i][1] and plansza[i][2] == plansza[i][1] and plansza[i][2] != 0:
+            return plansza[i][0]
+    for i in range(3):
+        if plansza[0][i] == plansza[1][i] and plansza[2][i] == plansza[1][i] and plansza[2][i] != 0:
+            return plansza[0][i]
+    if plansza[0][0] == plansza[1][1] and plansza[1][1] == plansza[2][2] and plansza[1][1] != 0:
+        return plansza[0][0]
+    if plansza[0][2] == plansza[1][1] and plansza[1][1] == plansza[2][0] and plansza[1][1] != 0:
+        return plansza[0][0]
+    czy_koniec_1 = True
+    for i in range(3):
+        for j in range(3):
+            print(plansza[i][j])
+            if plansza[i][j] == 0:
+                czy_koniec_1 = False
+    if czy_koniec_1:
+        return 0
+    else:
+        return 2
+
+
+def podaj_wyglad_planszy():
+    wyglad = ""
+    for i in range(3):
+        for j in range(3):
+            if plansza[i][j] == 0:
+                wyglad += " "
+            elif plansza[i][j] == 1:
+                wyglad += "O"
+            else:
+                wyglad += "X"
+    return wyglad
+
+
+def nasluchuj(socket):
+    data = b''
+    while data:
+        #może tutaj powinniśmy dać socket na który ma nasłuchiwać?
+        data += socket.recv(1)
+        print(data)
+    return data
+
+
+
+wykonaj_ruch(2, 1, 1)
+
+
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# context.load_cert_chain(r'C:\Users\patryk.krawczak\Downloads\server.crt', r'C:\Users\patryk.krawczak\Downloads\server.key')
+def create_context():
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_cert_chain(certfile=r'C:\Users\admin\Downloads\server.crt',
+                            keyfile=r'C:\Users\admin\Downloads\server.key')
+    context.load_verify_locations(cafile=r'C:\Users\admin\Downloads\client.crt')
+
+    return context
+
+
+context = create_context()
+
+dictionary_data_users = {}
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket, numer_gracza, session_id):
+        print("startuje")
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        self.numer_gracza = numer_gracza
+        self.session_id = session_id
+
+    def run(self):
+        while True:
+            resp = nasluchuj(self.csocket)
+            resp = resp.decode()
+            print(resp)
+            if resp[:16] == 'To:Server\r\nLogin':
+                login = resp[16:20]
+                global dictionary_data_users
+                dictionary_data_users[login] = {
+                    "session_id": self.session_id,
+                    "numer_gracza": self.numer_gracza
+                }
+                message = resp[48:-4]
+
+                if message == "START":
+                    mess = f'To:{login}\r\nsession_number:{self.session_id}\r\n\r\n'
+                    self.csocket.sendall(mess.encode())
+                    resp = nasluchuj()
+                    resp = resp.decode()
+                    if resp[-19:-4] != "BAD CREDENTIALS":
+                        msg = read_message(resp)
+                        To, From, Information_about_client_sesion_id, Message_id, Content_length, Message = msg
+                        if To == "SERWER" and From == str(login) and Information_about_client_sesion_id == str(
+                                self.session_id):
+                            msg = Message
+                            if msg == "i am ready":
+                                while True:
+                                    if aktualny_gracz != self.numer_gracza:
+                                        pass
+                                    else:
+                                        #Sprawdzam która gra się skończyła i wysyłam do jednego i drugiego klienta informacje o tym
+                                        if czy_koniec() == 0:
+                                            msg = opakuj(login,"SERWER",self.session_id,200,len("YOU WIN PLAYER 0" ),"YOU WIN PLAYER 0")
+                                            self.csocket.sendall(msg.encode())
+                                        elif czy_koniec() == 2:
+                                            zmien_gracza()
+                                            msg = opakuj(login, "SERWER", self.session_id, 200, len("YOU WIN PLAYER 0"),"YOU WIN PLAYER 0")
+                                            self.csocket.sendall(msg.encode())
+                                        #Jeśli gra się nie skończyła
+                                        else:
+                                            #Wyślij wiadomość do gracza  aby podał ruch wraz z aktualnym stanem planszy
+                                            msg_podaj_ruch = opakuj(login, "SERWER", self.session_id, 200, len("PODAJ RUCH"),"PODAJ RUCH")
+                                            self.csocket.sendall(msg_podaj_ruch.encode())
+                                            #Czeka na ruch klienta
+                                            resp = nasluchuj()
+                                            resp = resp.decode()
+                                            ruch = int(resp[-5:-4])
+                                            if ruch >=1 and ruch <=9:
+                                                x,y = tlumacz_na_x_y(ruch)
+                                                czy_udalo_sie = wykonaj_ruch(x,y,self.csocket)
+                                                if czy_udalo_sie == "pole zajete":
+                                                    msg_zly_ruch = opakuj(login,"SERWER",self.session_id,400,len("BAD MOVE"),"BAD MOVE")
+                                                    self.csocket.sendall(msg_zly_ruch.encode())
+                                                elif czy_udalo_sie == "udalo sie":
+                                                    msg_dobry_ruch = opakuj(login, "SERWER", self.session_id, 400,len("RIGHT MOVE"), "RIGHT MOVE")
+                                                    self.csocket.sendall(msg_dobry_ruch.encode())
+                                                    #Dobry ruch więc muszę zmienić gracza i powtórzyć pętle
+                                                    zmien_gracza()
+
+                                            # zwaliduj czy wiadomość jaka otrzymałeś jest ruchem czy jest to int od 1-9
+
+                                #### serwer sprawdza czy gra się nie skończyła i jeżeli się skończyła to informuje gracza jeżeli nie to:
+                                #### serwer wysyła wiadomość do gracza by podał ruch
+                                #### i przetwarza ten ruch, sprawdza czy legalny informuje że się powiódł ( lub że się nie powiódł)
+                                #### zmienić aktualnego gracza
+                                ####
+
+
+LOCALHOST = "127.0.0.1"
+PORT = 8082
+
+licznik_graczy = 0
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+    sock.bind(('127.0.0.1', PORT))
+    sock.listen(5)
+    with context.wrap_socket(sock, server_side=True) as ssock:
+
+        print("Server started")
+        while True:
+            if licznik_graczy < 2:
+                clientsock, clientAddress = ssock.accept()
+                Session_id = int(uuid.uuid4())
+                newthread = ClientThread(clientAddress, clientsock, aktualny_gracz_f(), Session_id)
+                newthread.start()
+                zmien_gracza()
+                licznik_graczy += 1
+
+## CLIENT
