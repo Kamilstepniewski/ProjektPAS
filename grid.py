3fbb5b02acb718da0f2ccb7b4769d04168dcfb21
Kamil Stępniewski <kamil.stepniewski@outlook.com>
2020-06-19 15:12:04 +0200

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,15 +1,21 @@
 import pygame
 import os
 
+pygame.init()
+
 letterX = pygame.image.load(os.path.join('kolko.png'))
 letterO = pygame.image.load(os.path.join('Krzyzyk.png'))
 
+font = pygame.font.SysFont("monospace", 30)
+label = font.render("New Game", 1, (0,0,0))
+
+
 class Grid:
     def __init__(self):
-        self.grid_lines = [((0,200), (600,200)), # first horizontal line
-                           ((0,400), (600,400)), # second horizontal line
-                           ((200,0), (200,600)), # first vertical line
-                           ((400,0), (400,600))] # second vertical line
+        self.grid_lines = [((15,200), (585,200)), # first horizontal line
+                           ((15,400), (585,400)), # second horizontal line
+                           ((200,15), (200,585)), # first vertical line
+                           ((400,15), (400,585))] # second vertical line
 
         self.grid = [[0 for x in range(3)] for y in range(3)]
         # search directions  N         NW        W       SW       S       SE      E       NE
@@ -19,6 +25,11 @@ class Grid:
     def draw(self, surface):
         for line in self.grid_lines:
             pygame.draw.line(surface, (200, 200, 200), line[0], line[1], 2)
+
+        font = pygame.font.SysFont("monospace", 30)
+        label = font.render("New Game", 1, (0, 0, 0))
+        pygame.draw.rect(surface, (255,255,255), (200, 600, 200, 100))
+        surface.blit(label, (225,620))
 
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
